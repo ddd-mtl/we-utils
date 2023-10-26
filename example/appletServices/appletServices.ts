54e8fb07964c7492dff5ef49df81807ceed2b1d1
damien <doute.d@gmail.com>
2023-10-26 15:59:40 -0400

    Initial import

---
+++ example/appletServices/appletServices.ts
@@ -0,0 +1,20 @@
+import {getEntryInfo} from "./getEntryInfo";
+import {AppletServices} from "@lightningrodlabs/we-applet";
+import {DevTestNames} from "../common/setup";
+import {attachmentTypes} from "./attachmentTypes";
+
+/** */
+export const threadsNames: DevTestNames = {
+    installed_app_id: "ThreadsWeApplet",
+    provisionedRoleName: "role_threads",
+}
+
+
+/** */
+export const appletServices: AppletServices = {
+    attachmentTypes,
+    //attachmentTypes: async (_appletClient) => ({}),
+    getEntryInfo,
+    blockTypes: {},
+    search: async (appletClient, searchFilter) => {return []},
+};
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {setup} from "./common/setup";
+import {threadsNames} from "./appletServices/appletServices";
+import {createThreadsApplet} from "./createThreadsApplet";
+
+
+export default {
+  setup,
+  createThreadsApplet,
+  threadsNames,
+};
+
+
